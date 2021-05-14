14ac38aa68f5a270754fd72f08ae559143389f65
ilyas ahmed <ilyasahmed081@gmail.com>
2021-05-15 02:21:51 +0500

    data showing through input decorator

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  list: any = [
+    {
+      id: 1,
+      image: 'https://un-prod-s3-bucket.s3.amazonaws.com/event_uvite_own_themes/image_thumbs/5048/4130/original/thumbnail.jpg',
+      name: '4th Annual Taco & Tequila Crawl: ATL',
+      start_date: 'May 15, 2021',
+      start_time: '1:00pm',
+      created_by: 'Eventbrite',
+      price: '$5.00'
+    },
+    {
+      id: 2,
+      image: 'https://un-prod-s3-bucket.s3.amazonaws.com/event_uvite_own_themes/image_thumbs/5048/4130/original/thumbnail.jpg',
+      name: 'Chastain Park Spring Arts & Crafts Festival 2021',
+      start_date: 'May 15, 2021',
+      start_time: '1:00pm',
+      created_by: 'Eventbrite',
+      price: '$5.00'
+    },
+    {
+      id: 3,
+      image: 'https://un-prod-s3-bucket.s3.amazonaws.com/event_uvite_own_themes/image_thumbs/5048/4130/original/thumbnail.jpg',
+      name: 'Fernbank After Dark: Eye Spy',
+      start_date: 'May 15, 2021',
+      start_time: '1:00pm',
+      created_by: 'Eventbrite',
+      price: '$5.00'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
--- src/app/shared/custom-card/custom-card.component.ts
+++ src/app/shared/custom-card/custom-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-custom-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./custom-card.component.scss']
 })
 export class CustomCardComponent implements OnInit {
+
+  @Input('data') data: any;
 
   constructor() { }
 
