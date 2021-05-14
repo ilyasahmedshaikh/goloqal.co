c2e13cb91f4e9f4a2bec93e83a60521707a0e3ff
ilyas ahmed <ilyasahmed081@gmail.com>
2021-05-14 18:38:42 +0500

    custom card init

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { CustomCardComponent } from './custom-card/custom-card.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CustomCardComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CustomCardComponent
   ]
 })
 export class SharedModule { }
