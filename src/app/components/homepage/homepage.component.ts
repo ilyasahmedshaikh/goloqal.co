001b5a38f21f03934dcd3656f1e7dd282d1dced2
ilyas ahmed <ilyasahmed081@gmail.com>
2021-05-15 14:39:14 +0500

    homepage done

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -7,34 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  list: any = [
+  events: any = [
     {
       id: 1,
-      image: 'https://un-prod-s3-bucket.s3.amazonaws.com/event_uvite_own_themes/image_thumbs/5048/4130/original/thumbnail.jpg',
-      name: '4th Annual Taco & Tequila Crawl: ATL',
+      image: 'https://prod-cdn.unation.com/orbweaver-prod/event-themes/40152/theme-1609057989.jpeg',
+      name: 'Musiq Soulchild With Special Guest Avery Sunshine',
       start_date: 'May 15, 2021',
-      start_time: '1:00pm',
-      created_by: 'Eventbrite',
-      price: '$5.00'
+      start_time: '7:00pm',
+      created_by: 'Ticketmaster',
+      price: '$45.00'
     },
     {
       id: 2,
-      image: 'https://un-prod-s3-bucket.s3.amazonaws.com/event_uvite_own_themes/image_thumbs/5048/4130/original/thumbnail.jpg',
-      name: 'Chastain Park Spring Arts & Crafts Festival 2021',
+      image: 'https://prod-cdn.unation.com/orbweaver-prod/event-themes/380490/theme-1613888143.jpeg',
+      name: 'ATLANTA-BARAK SHEKINAH LIVE 2021',
       start_date: 'May 15, 2021',
-      start_time: '1:00pm',
+      start_time: '7:00pm',
       created_by: 'Eventbrite',
-      price: '$5.00'
+      price: '$33.46'
     },
     {
       id: 3,
-      image: 'https://un-prod-s3-bucket.s3.amazonaws.com/event_uvite_own_themes/image_thumbs/5048/4130/original/thumbnail.jpg',
-      name: 'Fernbank After Dark: Eye Spy',
+      image: 'https://prod-cdn.unation.com/orbweaver-prod/event-themes/3202615/theme-1614407228.jpeg',
+      name: 'Atlanta Braves vs. New York Mets',
+      start_date: 'May 17, 2021',
+      start_time: '7:10pm',
+      created_by: 'Ticketmaster',
+      price: '$7.00'
+    }
+  ]
+  shops: any = [
+    {
+      id: 1,
+      image: 'https://prod-cdn.unation.com/orbweaver-prod/event-themes/3558223/theme-1616738086.jpeg',
+      name: 'Ready Set Grow Plant Exchange',
       start_date: 'May 15, 2021',
-      start_time: '1:00pm',
+      start_time: '2:00pm',
       created_by: 'Eventbrite',
-      price: '$5.00'
+      price: ''
     },
+    {
+      id: 2,
+      image: 'https://prod-cdn.unation.com/orbweaver-prod/event-themes/3569976/theme-1616825106.jpeg',
+      name: 'Exotic Car Show',
+      start_date: 'May 15, 2021',
+      start_time: '9:00am',
+      created_by: 'Eventbrite',
+      price: '$10.00'
+    },
+    {
+      id: 3,
+      image: 'https://prod-cdn.unation.com/orbweaver-prod/event-themes/1115185/theme-1609055086.jpeg',
+      name: 'Team Battle Als Walk',
+      start_date: 'May 15, 2021',
+      start_time: '8:00am',
+      created_by: 'Eventbrite',
+      price: '$25.00'
+    }
   ]
 
   constructor() { }
