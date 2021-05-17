e4475f5409ed073797bcfb9abae1426c5383d75c
ilyas ahmed <ilyasahmed081@gmail.com>
2021-05-17 16:54:07 +0500

    add categories and category listing screens done

--- src/app/components/categories/category-listing/category-listing.component.ts
+++ src/app/components/categories/category-listing/category-listing.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListingComponent implements OnInit {
 
+  data: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.data = [
+      {
+        id: 1,
+        categoryName: 'Category 1'
+      },
+      {
+        id: 2,
+        categoryName: 'Category 2'
+      }
+      ,
+      {
+        id: 3,
+        categoryName: 'Category 3'
+      },
+      {
+        id: 4,
+        categoryName: 'Category 4'
+      },
+      {
+        id: 5,
+        categoryName: 'Category 5'
+      },
+      {
+        id: 6,
+        categoryName: 'Category 6'
+      }
+    ]
   }
 
 }
