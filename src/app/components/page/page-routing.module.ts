a166daa869737997bd8d89bd27877519bdb77be2
ilyas ahmed <ilyasahmed081@gmail.com>
2021-05-18 16:09:00 +0500

    page components added in page module

--- src/app/components/page/page-routing.module.ts
+++ src/app/components/page/page-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PageComponent } from './page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { NearByPageComponent } from './near-by-page/near-by-page.component';
+import { PageDetailsComponent } from './page-details/page-details.component';
+import { PageListingComponent } from './page-listing/page-listing.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PageComponent,
+    children: [
+      {
+        path: '',
+        component: PageListingComponent
+      },
+      {
+        path: 'page-listing',
+        component: PageListingComponent
+      },
+      {
+        path: 'page-details',
+        component: PageDetailsComponent
+      },
+      {
+        path: 'create-page',
+        component: CreatePageComponent
+      },
+      {
+        path: 'nearby-page',
+        component: NearByPageComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
