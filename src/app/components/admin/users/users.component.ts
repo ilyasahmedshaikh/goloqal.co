a9fa682de1a5d512439e84b600b788f73c934540
ilyas ahmed <ilyasahmed081@gmail.com>
2021-05-24 14:21:25 +0500

    users table screen done

--- src/app/components/admin/users/users.component.ts
+++ src/app/components/admin/users/users.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  data: any = [
+    { id: 1,  name: "John", email: "john12@email.com", userType: "User"},
+    { id: 2,  name: "Michael", email: "michael02@email.com", userType: "User"},
+    { id: 3,  name: "Daniel", email: "daniel96@email.com", userType: "User"},
+    { id: 4,  name: "Kevin", email: "kevin52@email.com", userType: "User"},
+    { id: 5,  name: "Anthony", email: "anthony@email.com", userType: "User"},
+    { id: 6,  name: "Jason", email: "jason963@email.com", userType: "User"}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
