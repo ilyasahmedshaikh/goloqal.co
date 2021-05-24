18cc416f6626247be9541690a1afd2fd3f39f5ff
ilyas ahmed <ilyasahmed081@gmail.com>
2021-05-24 15:30:21 +0500

    topic listing screen done

--- src/app/components/admin/topics-listing/topics-listing.component.ts
+++ src/app/components/admin/topics-listing/topics-listing.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopicsListingComponent implements OnInit {
 
+  data: any = [
+    { id: 1,  name: "Pepsi", price: "$12.23", createdBy: "Pepsico"},
+    { id: 2,  name: "Burger", price: "$15.14", createdBy: "KFC"},
+    { id: 3,  name: "Lays", price: "$16.36", createdBy: "Lays"},
+    { id: 4,  name: "Pepsi", price: "$12.23", createdBy: "Pepsico"},
+    { id: 5,  name: "Burger", price: "$15.14", createdBy: "KFC"},
+    { id: 6,  name: "Lays", price: "$16.36", createdBy: "Lays"},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
