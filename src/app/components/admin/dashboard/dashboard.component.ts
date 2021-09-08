b4126770e0e618c4b0f2d8eba4a2ae7a4eda7426
Muhammad Awais <awaisshaikh94@gmail.com>
2021-09-08 15:11:56 +0500

    7. Admin portal for me to edit anything. This should be able to find from my account. I don’t know how to access it now

--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  adminControls: any = [
+    {name: "Category Listing", path: "/categories/category-listing", icon: "fas fa-th-list"},
+    {name: "Sliders", path: "/admin/add-edit-slider", icon: "fas fa-images"},
+    {name: "Users", path: "/admin/users", icon: "fas fa-users"},
+    {name: "Socials", path: "/admin/social-links", icon: "fas fa-share-alt"},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
