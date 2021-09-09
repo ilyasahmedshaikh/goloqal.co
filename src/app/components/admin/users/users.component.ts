1c2e1107f24a81fc01eca52f0fbd892210814480
Muhammad Awais <awaisshaikh94@gmail.com>
2021-09-09 22:46:11 +0500

    users - admin can edit info

--- src/app/components/admin/users/users.component.ts
+++ src/app/components/admin/users/users.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ConfigService } from '../../../core/http/config/config.service'
 import { ApiService } from '../../../core/http/api/api.service';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-users',
@@ -10,6 +12,10 @@ import { ApiService } from '../../../core/http/api/api.service';
 })
 export class UsersComponent implements OnInit {
 
+  @ViewChild('userModalClose') modalClose;
+
+  programForm: any = FormGroup;
+
   data: any = [
     { id: 1,  name: "John", email: "john12@email.com", userType: "User"},
   ];
@@ -17,17 +23,49 @@ export class UsersComponent implements OnInit {
   constructor(
     private config: ConfigService,
     private api: ApiService,
-    private router: Router
+    private router: Router,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.formInit();
     this.getUsers();
+  }
+
+  formInit() {
+    this.programForm = this.fb.group({
+      id:  ['', Validators.required],
+      fullName: ['', Validators.required],
+      email: ['', Validators.required],
+      user_type: ['', Validators.required],
+    });
   }
 
   getUsers() {
     this.api.getAll(this.config.collections.users_table).subscribe(res => {
       this.data = res;
     })
+  }
+
+  editUser(user) {
+    this.programForm.setValue({
+      id: user.id,
+      fullName: user.fullName,
+      email: user.email,
+      user_type: user.user_type,
+    })
+  }
+
+  updateUser() {
+    let request = this.api.put(this.config.collections.users_table, this.programForm.value.id, this.programForm.value);
+    request.then(() => {
+      alert('User Info Updated');
+      this.modalClose.nativeElement.click();
+      this.getUsers();
+    })
+    .catch((error) => {
+      alert(error);
+    });
   }
 
 }
