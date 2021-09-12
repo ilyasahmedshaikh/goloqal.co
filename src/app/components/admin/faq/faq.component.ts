43ac1ec5f34093729658ec48534732148b3a7864
Muhammad Awais <awaisshaikh94@gmail.com>
2021-09-12 22:54:05 +0500

    faq content and page dynamic

--- src/app/components/admin/faq/faq.component.ts
+++ src/app/components/admin/faq/faq.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfigService } from '../../../core/http/config/config.service';
+import { ApiService } from '../../../core/http/api/api.service';
 
 @Component({
   selector: 'app-faq',
@@ -7,9 +10,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  faqCount: number = 4;
+  content: any = [];
+
+  data: any = {};
+  isEdit: boolean = false;
+
+  constructor(
+    private config: ConfigService,
+    private api: ApiService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getContent();
+
+    for (let i = 0; i < this.faqCount; i++) {
+      this.content.push({
+        question: '',
+        answer: ''
+      })
+    }
+  }
+
+  getContent() {
+    this.api.getAll(this.config.collections.contentPages.faq).subscribe(res => {
+      this.data = res;
+      console.log(this.data);
+    });
+  }
+
+  addContent() {
+    let data = {
+      content: this.content
+    }
+    
+    let request = this.api.post(this.config.collections.contentPages.faq, data);
+
+    request.then(() => {
+      alert('Content Added.');
+      this.router.navigateByUrl("/faq");
+    })
+    .catch((error) => {
+      alert(error);
+    });
+  }
+
+  updateContent() {
+    let data = {
+      content: this.content
+    }
+    
+    let request = this.api.put(this.config.collections.contentPages.faq, this.data[0]?.id, data);
+
+    request.then(() => {
+      alert('Content Updated.');
+      this.router.navigateByUrl("/faq");
+    })
+    .catch((error) => {
+      alert(error);
+    });
+  }
+
+  onEdit() {
+    this.isEdit = !this.isEdit;
+    this.content = this.data[0].content;
+  }
+
+  onCancel() {
+    this.isEdit = !this.isEdit;
   }
 
 }
--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../../core/http/config/config.service';
+import { ApiService } from '../../core/http/api/api.service';
 
 @Component({
   selector: 'app-faq',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  data: any = {};
+  selected: number = 0;
+
+  constructor(
+    private config: ConfigService,
+    private api: ApiService,
+  ) { }
 
   ngOnInit(): void {
+    this.getContent();
+  }
+
+  getContent() {
+    this.api.getAll(this.config.collections.contentPages.faq).subscribe(res => {
+      this.data = res;
+      console.log(this.data);
+    });
+  }
+
+  onAccordian(i) {
+    this.selected = i;
   }
 
 }
