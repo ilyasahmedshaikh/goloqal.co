2edf6a4c5a5dd45be093900bf12feebff8b78cfb
Muhammad Awais <awaisshaikh94@gmail.com>
2021-09-12 23:00:45 +0500

    Partner with us dynamic content done

--- src/app/components/admin/partner-with-us/partner-with-us.component.ts
+++ src/app/components/admin/partner-with-us/partner-with-us.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfigService } from '../../../core/http/config/config.service';
+import { ApiService } from '../../../core/http/api/api.service';
 
 @Component({
   selector: 'app-partner-with-us',
@@ -7,9 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PartnerWithUsComponent implements OnInit {
 
-  constructor() { }
+  content: string = "";
+  data: any = {};
+  isEdit: boolean = false;
+
+  constructor(
+    private config: ConfigService,
+    private api: ApiService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getContent();
+  }
+
+  getContent() {
+    this.api.getAll(this.config.collections.contentPages.partnerWithUs).subscribe(res => {
+      this.data = res;
+      console.log(this.data);
+    });
+  }
+
+  addContent() {
+    let data = {
+      content: this.content
+    }
+    
+    let request = this.api.post(this.config.collections.contentPages.partnerWithUs, data);
+
+    request.then(() => {
+      alert('Content Added.');
+      this.router.navigateByUrl("/partner-with-us");
+    })
+    .catch((error) => {
+      alert(error);
+    });
+  }
+
+  updateContent() {
+    let data = {
+      content: this.content
+    }
+    
+    let request = this.api.put(this.config.collections.contentPages.partnerWithUs, this.data[0]?.id, data);
+
+    request.then(() => {
+      alert('Content Updated.');
+      this.router.navigateByUrl("/partner-with-us");
+    })
+    .catch((error) => {
+      alert(error);
+    });
+  }
+
+  onEdit() {
+    this.isEdit = !this.isEdit;
+    this.content = this.data[0].content;
+  }
+
+  onCancel() {
+    this.isEdit = !this.isEdit;
   }
 
 }
