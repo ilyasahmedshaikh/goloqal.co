83acb1b62dd18f7d320c42e7c3fc4050763af3e4
Muhammad Awais <awaisshaikh94@gmail.com>
2021-11-07 17:22:04 +0500

    changes revert for keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,7 @@
 export const environment = {
   production: true,
-  google_maps_api: 'AIzaSyDZ0egMckxew1PLcfQdN_XkVbW73lF_PSg',
+  google_maps_api: 'YOUR_GOOGLE_MAPS_API_KEY',
   firebase: {
-    apiKey: "AIzaSyBgV7EwK83JrAd_TRzkQGQ3hugIBkjxC0I",
-    authDomain: "goloqal-dc7bf.firebaseapp.com",
-    projectId: "goloqal-dc7bf",
-    storageBucket: "goloqal-dc7bf.appspot.com",
-    messagingSenderId: "544164490700",
-    appId: "1:544164490700:web:70cdbfd859425311114169",
-    measurementId: "G-K2P6VHRHV6"
+    // your firebase object
   }
 };
